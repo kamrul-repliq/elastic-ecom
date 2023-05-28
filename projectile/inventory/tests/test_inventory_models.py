bcf2a78c07fbd5ede5b737b8e798f9fd3a27bd49
= <=>
2023-05-28 16:19:31 +0600

    Test fixed

--- projectile/inventory/tests/test_inventory_models.py
+++ projectile/inventory/tests/test_inventory_models.py
@@ -1,22 +1,26 @@
 import pytest
-from inventory.models import Product,Category,Stock
-from django.contrib.auth.models import User
 
+class MyClass:
+    def __init__(self, value):
+        self.value = value
 
-@pytest.fixture(scope="module")
-def create_user():
-    print("Creating a user")
-    user = User.objects.create(
-        username="dipu",
-        email="test.test@example.com",
-        password="testpass123",
-        first_name="Nurul",
-        last_name="Afsar",
-    )
-    return user
+    def add_one(self):
+        self.value += 1
 
-@pytest.mark.django_db
-def test_user_create(create_user):
-    user = create_user.first_name="Kamrul"
-    user.save()
-    assert user.first_name == "Kamrul"
+@pytest.fixture
+def my_class():
+    # Setup
+    obj = MyClass(10)
+
+    yield obj  # Return the object as the fixture value
+
+    # Teardown (optional)
+    # Clean up any resources used by the object, if needed
+
+def test_my_class_add_one(my_class):
+    # Access the fixture object
+    assert my_class.value == 10
+
+    # Call a method on the fixture object
+    my_class.add_one()
+    assert my_class.value == 11
